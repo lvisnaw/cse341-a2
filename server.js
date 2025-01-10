e02f5e5847007cdcdab52b12d5de136250b8a2f7
lvisnaw <lvisnaw@gmail.com>
2025-01-09 20:38:30 -0700

    Attempting to build a web app

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use('/', require('./routes'));
+// app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-})
+app.get("/", (req, res) => {
+    res.send("Hello Stephanie Visnaw!");
+});
+
+app.listen(process.env.PORT || port);
+console.log("Web Server is listening on port " + (process.env.PORT || port));
+
+// app.listen(port, () => {
+//     console.log(`Running on port ${port}`);
+// })
