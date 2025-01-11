822fb8ac3c236f5ac46740189d006d6260fbdd14
lvisnaw <lvisnaw@gmail.com>
2025-01-11 13:41:16 -0700

    Site isn't woring anymore - troubleshooting

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const indexController = require("./controllers/index");
+
 
 app.use('/', require('./routes'));
 
 app.listen(process.env.PORT || port);
-console.log("Web Server is listening on port " + (process.env.PORT || port));
+console.log("Web Server is listening on port " + (process.env.port || port));
 
 // app.listen(port, () => {
 //     console.log(`Running on port ${port}`);
